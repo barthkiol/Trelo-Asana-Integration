31fa02493b5bd096157c23dde6d59012885b497b
Gabriel Barth <gabriel.barth@neomode.com.br>
2024-07-14 18:27:07 -0300

    Repo commit

--- Aplicacao integracao/Models/Trello/Models.cs
+++ Aplicacao integracao/Models/Trello/Models.cs
@@ -3,24 +3,5 @@
 
 namespace Aplicacao_integracao.Models.Trello
 {
-    public sealed record TrelloBoard
-    (
-        [property: JsonPropertyName("id")] string Id,
-        [property: JsonPropertyName("name")] string Name
-    );
-    public sealed record TrelloList
-    (
-        [property: JsonPropertyName("id")] string Id,
-        [property: JsonPropertyName("name")] string Name
-    );
-    public sealed record TrelloCard
-    (
-        [property: JsonPropertyName("name")] string Name,
-        [property: JsonPropertyName("closed")] bool Closed,
-        [property: JsonPropertyName("desc")] string Description,
-        [property: JsonPropertyName("assignee_section")] string Section,
-        [property: JsonPropertyName("start")] DateTime Start,
-        [property: JsonPropertyName("due")] DateTime Due
-    );
-}
+    }
 
